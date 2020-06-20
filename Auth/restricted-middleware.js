4d687926d31f012f477e899db47ba3b217a15e5c
Seth Nelson <seth.nelson@outlook.com>
2020-06-20 14:47:19 -0700

    reorganized files and structure. deleted the validation  and string verification that is not needed.

--- Auth/restricted-middleware.js
+++ Auth/restricted-middleware.js
@@ -1,30 +1,27 @@
 // -- Use JWT and secrets to verify user
-// const jwt = require('jsonwebtoken');
-// const secrets = require('../config/secrets.js');
+const jwt = require('jsonwebtoken');
 
+// -- Method to verify that an auth token is:
+// 1) Included as a header
+// 2) Is valid
+// 3) Not expired (time limit based on options)
 
-// // -- Method to verify that an auth token is:
-// // 1) Included as a header
-// // 2) Is valid
-// // 3) Not expired (time limit based on options)
+module.exports = (req, res, next) => {
+    const { authorization } = req.headers;
 
-// module.exports = (req, res, next) => {
-//     try {
-//         const token = req.headers.authorization.split(" ")[1];
-//         if (token) {
-//             jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
-//                 if (err) {
-//                     res.status(401).json({ errorMessage: 'Token cannot be verified.'});
-//                 } else {
-//                     req.decodedJwt = decodedToken;
-//                     console.log(req.decodedJwt);
-//                     next();
-//                 }
-//             });
-//         } else {
-//             throw new error('Auth data is invalid.');
-//         }
-//     } catch (err) {
-//         res.status(401).json({ error: err.message });
-//     }
-// };
+    if (authorization) {
+        const secret = process.env.JWT_SECRET || 'air is a liquid';
+
+        jwt.verify(authorization, secret, function(err, decodedToken) {
+            if (err) {
+                res.status(401).json({ errorMessage: 'Token is invalid.' });
+            } else {
+                req.token = decodedToken;
+
+                next();
+            }
+        });
+    } else {
+        res.status(400).json({ errorMessage: 'Please login and try again.' })
+    }
+};
